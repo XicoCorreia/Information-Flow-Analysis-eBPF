fa59c53dd8e95d274afacb90bf30a65a6cf27da8
XicoCorreia <xicocorreia17@gmail.com>
2025-01-02 15:55:43 +0100

    ebpfProgram test

---
+++ ebpfPrograms/firstProg.c
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause) */
+#define BPF_NO_GLOBAL_DATA
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+typedef unsigned int u32;
+typedef int pid_t;
+const pid_t pid_filter = 0;
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
+
+SEC("tp/syscalls/sys_enter_write")
+int handle_tp(void *ctx)
+{
+ return 0;
+}
