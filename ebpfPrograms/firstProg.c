10586b11da37a2a063fa89bc2f81551dce7ec846
XicoCorreia <xicocorreia17@gmail.com>
2025-01-02 17:06:54 +0100

    Great success

--- ebpfPrograms/firstProg.c
+++ ebpfPrograms/firstProg.c
@@ -13,5 +13,9 @@ char LICENSE[] SEC("license") = "Dual BSD/GPL";
 SEC("tp/syscalls/sys_enter_write")
 int handle_tp(void *ctx)
 {
+ pid_t pid = bpf_get_current_pid_tgid() >> 32;
+ if (pid_filter && pid != pid_filter)
+  return 0;
+ bpf_printk("BPF triggered sys_enter_write from PID %d.\n", pid);
  return 0;
 }
